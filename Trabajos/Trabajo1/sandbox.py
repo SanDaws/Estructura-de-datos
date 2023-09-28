99a37b419d5ef7c21ed62687e086ff746af73b73
Daws <75854211+SanDaws@users.noreply.github.com>
2023-09-27 19:02:19 -0500

    Update sandbox.py

--- Trabajos/Trabajo1/sandbox.py
+++ Trabajos/Trabajo1/sandbox.py
@@ -5,20 +5,24 @@ pygame.init()
 screen = pygame.display.set_mode((720, 480))
 clock = pygame.time.Clock()
 running = True
-
+blanco=pygame.Color(255,255,255)
+rectanglo= pygame.Rect(1,1,120,200)
 while running:
+    screen.fill("purple")
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    pygame.draw.rect(screen, blanco, rectanglo, 12)#x,y,base,altura), grosor de la linea(null=relleno)
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    
 
     # RENDER YOUR GAME HERE
 
-    # flip() the display to put your work on screen
+
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60

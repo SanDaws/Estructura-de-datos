d5612ccf26bd306690ae31363ff8773e616f0e18
Daws <75854211+SanDaws@users.noreply.github.com>
2023-10-11 15:24:20 -0500

    organizado para trabajar

---
+++ sem6/paint2yo.py
@@ -0,0 +1,15 @@
+"""Sí, leíste bien. Este ejercicio consiste en determinar si se inserta, uno a uno, 
+una serie de elementos en un árbol binario de búsqueda, ¿cómo sería el árbol resultante?
+Y al decir "cómo sería" quiere decir que tu programa debe dibujarlo rotado 90° en sentido
+ anti-horario. Eso significa que en cada línea de "dibujará" un único nodo y deberá usarse 
+una tabulació por cada nivel del árbol que se descienda. La raíz por tanto no tendrá ninguna 
+tabulación. Para mayor claridad ver los ejemplos.
+Input
+La entrada comienza con una línea que contiene un número entero positivo C que corresponde
+ a la cantidad C de casos, no más de 50. Luego siguen C líneas, cada una con no menos de 1 y 
+ no más de 64 valores enteros no negativos, menores a 100, sin repetidos, y separados entre sí 
+ por un espacio en blanco. Al final de cada línea hay un valor -1 que no hace parte de la serie 
+ correspondiente.
+Output
+La salida debe contener por cada caso de prueba el dibujo correspondiente del árbol. Entre árbol 
+y árbol se debe dejar una línea vacía para separarlos."""
